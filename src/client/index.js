7bf02c2f0657644f5f778173ea134a537022bfb5
Yui-Iida <yxuxi328@icloud.com>
2022-05-25 11:00:40 +0200

    first commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import { checkForName } from './js/nameChecker';
 import { handleSubmit } from './js/formHandler';
+import './styles/resets.scss';
+import './styles/base.scss';
 
 console.log(checkForName);
 console.log(handleSubmit);
